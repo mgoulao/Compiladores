288004e47d5ad4d12f7db43123f9523e58fcdb92
Manuel Goulão <msilvagoulao@gmail.com>
2020-05-22 11:48:26 +0100

    fix public variables definition

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 
 make
 ./minor ./exs/test.min
-./minor ./exs/iter.min
+./minor ./exs/module.min
 nasm -felf32 exs/test.asm
-nasm -felf32 exs/iter.asm
-ld -m elf_i386 exs/test.o -L/home/mgoulao/Projects/Compiladores/minor -lminor
+nasm -felf32 exs/module.asm
+ld -m elf_i386 exs/test.o exs/module.o -L/home/mgoulao/Projects/Compiladores/minor -lminor
 
 
